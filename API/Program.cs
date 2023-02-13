65c5122ebf5d9f3ebc45e1d33d10cb86290792a2
Krisztian Kalmar <kalmarytb@gmail.com>
2023-02-13 22:37:45 +0000

    Walking skeleton 2 finished

--- API/Program.cs
+++ API/Program.cs
@@ -12,9 +12,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DeafultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
