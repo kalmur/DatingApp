094877bfbed30c83494f83c2634768dadbbd4359
Krisztian Kalmar <kalmarytb@gmail.com>
2023-02-25 16:23:40 +0000

    Reactive forms for register + validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,13 +1,13 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
-using API.Data.Migrations;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
+
 
 namespace API.Controllers
 {
@@ -15,8 +15,10 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
-        public AccountController(DataContext context, ITokenService tokenService)
+        private readonly IMapper _mapper;
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
+            _mapper = mapper;
             _tokenService = tokenService;
             _context = context;
         }
@@ -26,14 +28,14 @@ namespace API.Controllers
         {
             if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
+            //From -> to
+            var user = _mapper.Map<AppUser>(registerDto);
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser 
-            { 
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -41,7 +43,8 @@ namespace API.Controllers
             return new UserDTO
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -67,7 +70,8 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };
         }
 
--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -1,15 +1,22 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.DTOs
 {
     public class RegisterDTO
     {
-        [Required]
-        public string Username { get; set; }
+        [Required] public string Username { get; set; }
+
+        [Required] public string KnownAs { get; set; }
+
+        [Required] public string Gender { get; set; }
+
+        // optional -> only way it works
+        [Required] public DateOnly? DateOfBirth { get; set; }
+
+        [Required] public string City { get; set; }
+
+        [Required] public string Country { get; set; }
+
         
         [Required]
         [StringLength(8, MinimumLength = 4)]
