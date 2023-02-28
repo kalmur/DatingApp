f6690294652bf05765b11084a0242d4a6894e7ee
Krisztian Kalmar <kalmarytb@gmail.com>
2023-02-28 15:47:43 +0000

    Like User feature + pagination

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -91,6 +91,20 @@ export class MembersService {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
+  addLike(username: string) {
+    // Pass up empty object bcz of post method
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('predicate', predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
+  }
+
+
   private getPaginatedResult<T>(url: string, params: HttpParams) {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
